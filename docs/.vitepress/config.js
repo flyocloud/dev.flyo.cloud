d8c49c8c7ff8147e710692277a53b04904993685
Nadar <git@nadar.io>
2024-01-19 11:46:41 +0000

    WYSIWYG

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -67,7 +67,7 @@ export default {
             { text: 'Content Pools', link: '/dev/infos/pools'},
             { text: 'Inhalt Mapping', link: '/dev/infos/mapping'},
             { text: 'Rate Limit', link: '/dev/infos/ratelimit'},
-            { text: 'Wysiwyg Editor', link: '/dev/infos/wysiwyg'},
+            { text: 'WYSIWYG Editor', link: '/dev/infos/wysiwyg'},
           ]
         },
         {
